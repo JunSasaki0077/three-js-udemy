321b00d17df14b37086021425610f88e67f5fa87
Junsasaki0077 <ikasas0077@gmail.com>
2022-03-10 23:21:46 +0900

    カメラ,シーン,レンダラーを追加

--- three-example/main.js
+++ three-example/main.js
@@ -1,3 +1,18 @@
 import * as THREE from "three";
 
-console.log(THREE);
+let scene, camera, renderer;
+
+scene = new THREE.Scene();
+
+camera = new THREE.PerspectiveCamera(
+  50,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+renderer = new THREE.WebGLRenderer();
+document.body.appendChild(renderer.domElement);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+renderer.render(scene, camera);
